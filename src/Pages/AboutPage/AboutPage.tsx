be5233a9e52e52cabfd2b25092df01d2cbf74b53
michael.peter.joyce@gmail.com <michael.peter.joyce@gmail.com>
2019-01-27 19:20:15 +0000

    fixed about me section being too wide

--- src/Pages/AboutPage/AboutPage.tsx
+++ src/Pages/AboutPage/AboutPage.tsx
@@ -4,7 +4,7 @@ import './AboutPage.scss'
 export default class AboutPage extends Component {
     render() {
         return (
-            <div className="about-page">
+            <div className="container about-page">
                 <div className="content-grid">
                     <h1>About Me</h1>
                     <div className="about-wrapper">
